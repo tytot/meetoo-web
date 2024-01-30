75bbe64bbaa450da4f3f1da9d0f89e8c8b64b842
Tyler Lin <tylerhlin@gmail.com>
2024-01-30 11:32:37 -0500

    Add icon to join beta button

--- components/join-beta-button.tsx
+++ components/join-beta-button.tsx
@@ -1,11 +1,14 @@
 import { appStoreUrl } from '@/lib/constants'
 import Link from 'next/link'
 import { Button } from './ui/button'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAppStore } from '@fortawesome/free-brands-svg-icons'
 
 export default function JoinBetaButton({ secondary }: { secondary?: boolean | undefined }) {
     return (
         <Button variant={secondary ? 'secondary' : 'default'} asChild>
             <Link href={appStoreUrl} target="_blank">
+                <FontAwesomeIcon icon={faAppStore} size="lg" className="mr-2" />
                 Join the Beta
             </Link>
         </Button>
