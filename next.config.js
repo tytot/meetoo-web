75113f4891ee0352d1ce005e31fec14790567853
Tyler Lin <tylerhlin@gmail.com>
2024-03-07 14:49:58 -0500

    Add security headers

--- next.config.js
+++ next.config.js
@@ -10,6 +10,22 @@ const nextConfig = {
             },
         ],
     },
+    async headers() {
+        return [
+            {
+                key: 'X-Frame-Options',
+                value: 'SAMEORIGIN',
+            },
+            {
+                key: 'X-Content-Type-Options',
+                value: 'nosniff',
+            },
+            {
+                key: 'Referrer-Policy',
+                value: 'no-referrer-when-downgrade',
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig
