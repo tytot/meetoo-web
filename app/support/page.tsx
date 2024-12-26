9ec6735915d6973e67a9f73b62e10b901b2ad5b7
Tyler Lin <tylerhlin@gmail.com>
2024-12-26 00:09:38 -0500

    Cringe

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -16,6 +16,9 @@ export default function Support() {
                     </a>{' '}
                     with any support inquiries.
                 </p>
+                <p>
+                    If you would like to request for your account to be deleted, please email the address above and we will guide you through the process. Upon account deletion, your profile and all associated data will be irreversibly removed from our servers with no additional retention period.
+                </p>
             </article>
         </main>
     )
